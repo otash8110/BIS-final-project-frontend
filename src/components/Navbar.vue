cef3ecadb1fbba3c85efc133ff85bf650fc3a869
otash8110 <otash_baxadirov@mail.ru>
2023-01-25 18:14:34 +0500

    init project with some basic components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar class="justify-space-between">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-title>
+            UzExpoMarket
+        </v-app-bar-title>
+        <v-row class="flex-grow-1 v-col-8">
+            <v-col>
+                <v-text-field label="Search for products" hide-details single-line class="flex-grow-1"></v-text-field>
+            </v-col>
+            <v-col cols="2" class="d-flex align-center">
+                <v-btn icon="mdi-magnify"></v-btn>
+            </v-col>
+        </v-row>
+            <user-profile-menu></user-profile-menu>
+    </v-app-bar>
+</template>
+
+<script>
+import UserProfileMenu from './User/UserProfileMenu.vue'
+export default {
+  components: { UserProfileMenu },
+    name: "Nav-bar"
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import { HubConnectionBuilder, HttpTransportType } from "@microsoft/signalr";
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import router from './router'
 
-let token =
-  "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJhZG1pbmlzdHJhdG9yQGxvY2FsaG9zdCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiJhZG1pbmlzdHJhdG9yQGxvY2FsaG9zdCIsIlVzZXJJZCI6IjAiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsImV4cCI6MTY3NDE2MjUwM30.aEknEZzoB7rGZtob3AxDCKOnReSAZdRGlW09W1H0eKEABaqWoyfnYoSHbwCxkh-3Hmvxgq8hBKS9srUIzD90lw";
+loadFonts()
 
-let connection = new HubConnectionBuilder()
-  .withUrl("https://localhost:7083/hub/demohub", {
-    skipNegotiation: true,
-    transport: HttpTransportType.WebSockets,
-    accessTokenFactory: () => token,
-  })
-  .build();
-
-connection.on("send", (data) => {
-  console.log(data);
-});
-
-connection.start().then(() => {
-  console.log("CONNECTED");
-  connection.invoke("SendMessage", "Hello");
-});
-
-createApp(App).mount("#app");
+createApp(App).use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
